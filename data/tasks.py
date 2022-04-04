8625d9e0434441739b190a5152b68d8d3ee146a5
Vladimir Grigorovich <37683474+grigvlwork@users.noreply.github.com>
2022-04-04 11:38:30 +0700

    добавил таблицу с заданиями

---
+++ data/tasks.py
@@ -0,0 +1,14 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Task(SqlAlchemyBase):
+    __tablename__ = 'tasks'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    topic_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    board = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    background = sqlalchemy.Column(sqlalchemy.String, nullable=True)
